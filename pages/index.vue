fad791eaa9296dd43aedeeaec9f368fc8a4c6699
Aleksandr Iushko <aleksandriushko@gmail.com>
2021-10-14 07:32:24 +0300

    set home-start-up template

--- pages/index.vue
+++ pages/index.vue
@@ -1,71 +1,56 @@
 <template>
+    
     <div class="main-container">
 
-        <Header />
+        <HeaderBlack />
 
         <OffCanvasMobileMenu />
 
         <SearchPopup />
 
-        <HeroBusiness />
+        <HeroStartUp />
 
-        <ServiceOne id="service" />
+        <FeatureStartUp />
 
-        <PortfolioOne />
+        <WhatWeDo />
 
-        <CounterUpOne />
+        <EffectiveSolution />
 
-        <TeamOne />
+        <StartUpStratagy />
 
-        <CurveShape>
-            <template v-slot:bgcolor>
-                <path style="fill: #f5f5f5;
-                    stroke-miterlimit:10;" 
-                    d="M3360.5,97.739c-242,0-480-48.375-480-48.375
-                        S2647.5,0.5,2400.5,0.5s-480,48.375-480,48.375s-238,48.864-480,48.864s-480-48.375-480-48.375S727.5,0.5,480.5,0.5
-                        S0.5,48.875,0.5,48.875V108h1920h1920V48.875C3840.5,48.875,3602.5,97.739,3360.5,97.739z"/>
-            </template>
-        </CurveShape>
-        <TestimonialOne />
+        <PortfolioItemFilter />
 
-        <BrandLogoCarousel />
+        <NewsletterCta />
 
-        <BlogOne />
-
-        <ContactUsOne />
-
-        <Footer />
+        <FooterTwo />
 
     </div>
+
 </template>
 
 <script>
-
     export default {
         components: {
-            Header: () => import('@/components/Header'),
+            HeaderBlack: () => import('@/components/HeaderBlack'),
             OffCanvasMobileMenu: () => import('@/components/OffCanvasMobileMenu'),
             SearchPopup: () => import('@/components/SearchPopup'),
-            HeroBusiness: () => import('@/components/HeroBusiness'),
-            ServiceOne: () => import('@/components/sections/ServiceOne'),
-            PortfolioOne: () => import('@/components/sections/PortfolioOne'),
-            CounterUpOne: () => import('@/components/sections/CounterUpOne'),
-            TeamOne: () => import('@/components/sections/TeamOne'),
-            TestimonialOne: () => import('@/components/sections/TestimonialOne'),
-            BrandLogoCarousel: () => import('@/components/BrandLogoCarousel'),
-            BlogOne: () => import('@/components/sections/BlogOne'),
-            CurveShape: () => import('@/components/CurveShape'),
-            ContactUsOne: () => import('@/components/ContactUsOne'),
-            Footer: () => import('@/components/Footer'),
+            HeroStartUp: () => import('@/components/HeroStartUp'),
+            FeatureStartUp: () => import('@/components/sections/FeatureStartUp'),
+            WhatWeDo: () => import('@/components/WhatWeDo'),
+            EffectiveSolution: () => import('@/components/EffectiveSolution'),
+            StartUpStratagy: () => import('@/components/sections/StartUpStratagy'),
+            PortfolioItemFilter: () => import('@/components/sections/PortfolioItemFilter'),
+            NewsletterCta: () => import('@/components/NewsletterCta'),
+            FooterTwo: () => import('@/components/FooterTwo'),
         },
 
         mounted () {
-            document.body.classList.add('template-color-1', 'template-font-2')
+            document.body.classList.add('template-color-5', 'template-font-1')
         },
 
         head() {
             return {
-                title: 'Brook || Home Business'
+                title: 'Brook || Home Start Up'
             }
         },
     };
